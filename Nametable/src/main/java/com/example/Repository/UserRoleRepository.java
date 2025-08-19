3d970b75d9feda14f9165b6e8f14a0bf21774676
HajimeIto <HajimeIto@Sohia>
2025-08-19 18:32:22 +0900

    主キーを複合キーに変更

--- Nametable/src/main/java/com/example/Repository/UserRoleRepository.java
+++ Nametable/src/main/java/com/example/Repository/UserRoleRepository.java
@@ -3,7 +3,8 @@ package com.example.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.Entity.UserRole;
+import com.example.Entity.UserRoleId;
 
-public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
+public interface UserRoleRepository extends JpaRepository<UserRole, UserRoleId> {
 
 }
